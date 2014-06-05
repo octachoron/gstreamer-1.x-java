fc11ab225b65b9457d4848867f0a71221f5d9ac2
Roland Elek <elek.roland@gmail.com>
2014-06-05 21:04:48 +0200

    Remove macports specific files
    To keep the repository clean, this commit removes the macports
    specific build script and jnaerator files for now, and renames the
    only remaining jnaerator file to the recommended name
    (config.jnaerator).

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-java -jar jnaerator.jar glib.jnaerator -mode Directory -f -v
-
-java -jar jnaerator.jar gobject.jnaerator -mode Directory -f -v
-
-java -jar jnaerator.jar gio.jnaerator -mode Directory -f -v
