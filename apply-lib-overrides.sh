3c75c92c88f5f9ac7127f547fef52cec53ca7c04
Roland Elek <elek.roland@gmail.com>
2014-06-30 18:04:57 +0200

    Add some verbosity to apply-lib-overrides.sh

--- apply-lib-overrides.sh
+++ apply-lib-overrides.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+echo 'Applying auto-detected overrides and skip patterns to JNAerator config template...'
 sed -e "/\/\/!probed-lib-overrides/r _lib_overrides.in" \
     -e "//d" config.template.jnaerator > config.jnaerator
+echo 'Done'
