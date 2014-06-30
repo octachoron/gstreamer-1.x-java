8d7b55bf7542e4bce80492b1b09f1cb51b8fc486
Roland Elek <elek.roland@gmail.com>
2014-06-30 17:53:06 +0200

    Beautify logging in gen-lib-overrides.sh

--- gen-lib-overrides.sh
+++ gen-lib-overrides.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo 'Probing libraries for unexpected symbol placements...'
+
 overrides_file=$PWD/_lib_overrides.in
 mappings_file=$PWD/_lib_mappings.tmp
 
@@ -18,9 +20,11 @@ done
 
 multiarch_tuple=`gcc -print-multiarch 2>/dev/null`
 if [ $? -ne 0 ]; then
+  echo 'Platform uses FHS-style multiarch'
   # Fall back to /usr/lib or /usr/lib64
   [ "`uname -m`" == "x86_64" ] && libdir=/usr/lib64 || libdir=/usr/lib
 else
+  echo 'Platform uses Debian-style multiarch'
   libdir=/usr/lib/$multiarch_tuple
 fi
 
@@ -28,7 +32,7 @@ for lib_so_file in $lib_so_files; do
   lib_so_paths="$lib_so_paths $libdir/$lib_so_file"
 done
 
-echo "Libs @ $lib_so_paths"
+echo "Shared libraries searched: $lib_so_paths"
 
 nm -D --defined-only --print-file-name $lib_so_paths | tr ':' ' ' \
     | cut -d ' ' -f 1,4 > $mappings_file
@@ -42,10 +46,10 @@ for lib in $libs; do
     fun_lib=`egrep "^[^ ]+ $fun$" $mappings_file | cut -d ' ' -f 1`
     if [ "$fun_lib" != "$lib_so" ]; then
       if [ -z "$fun_lib" ]; then
-        echo "HIT: $fun is not exported"
+        echo "$fun is not exported in any of the libs, adding skip pattern"
         echo "-skipFunctions $fun" >> $overrides_file
       else
-        echo "HIT: $fun is in $fun_lib, not $lib_so"
+        echo "$fun is exported in $fun_lib instead of $lib_so, adding override"
         fun_lib_short=`echo $fun_lib \
           | sed -r "s@$libdir/lib([a-z]+)$lib_so_postfix@\1@g"`$lib_so_ver
         if [ -z "$lib_overrides" ]; then
@@ -61,3 +65,5 @@ done
 echo "-libraryOverrides $lib_overrides" >> $overrides_file
 
 popd >/dev/null
+
+echo "Finished probing for unexpected symbol placements"
